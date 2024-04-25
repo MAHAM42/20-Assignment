858e3ba5aee17394226d23d2da2974cc53eb6362
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 07:54:14 -0700

    20-Assignment
    TypeScript 45-Assignment of Q20 with Node.js

---
+++ main20.js
@@ -0,0 +1,14 @@
+"use strict";
+// ASSIGNMENT NO 20:
+/*
+Think of something you could store in a array. For example,
+you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.
+*/
+console.log("ASSIGNMANT NO 20:");
+// Making a programming Languages Array:
+let programmingLanguages = ["Typescript", "JavaScript", "Python", "PHP"];
+// Print the Message of List :
+console.log("List of Programming Languages:");
+// Print the Values of Array in the form of List:
+programmingLanguages.forEach(language => console.log(language));
---
+++ main20.ts
@@ -0,0 +1,18 @@
+// ASSIGNMENT NO 20:
+
+/*
+Think of something you could store in a array. For example, 
+you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.
+*/
+
+console.log("ASSIGNMANT NO 20:");
+
+// Making a programming Languages Array:
+let programmingLanguages: string[] = ["Typescript", "JavaScript", "Python", "PHP"];
+
+// Print the Message of List :
+console.log("List of Programming Languages:");
+
+// Print the Values of Array in the form of List:
+programmingLanguages.forEach(language => console.log(language));
